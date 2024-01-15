d1452f6ce4e650418a47a4c4c0e414655c3f3448
paresiqbal <thecrowdel@gmail.com>
2024-01-15 22:51:46 +0700

    Return multiply value

--- new.go
+++ new.go
@@ -2,29 +2,26 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
-func cycleName(n []string, f func(string)) {
-	for _, value := range n {
-		f(value)
+func getInit(name string) (string, string) {
+	s := strings.ToUpper(name)
+	names := strings.Split(s, " ")
+
+	var initials []string
+	for _, value := range names {
+		initials = append(initials, value[:1])
 	}
-}
 
-func greet(n string) {
-	fmt.Printf("Hello %v \n", n)
-}
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
 
-func circle(r float64) float64 {
-	return math.Pi * r * r
+	return initials[0], "_"
 }
 
 func main() {
-	cycleName([]string{"Pares", "Paul", "Paulo"}, greet)
-
-	a1 := circle(10.5)
-	a2 := circle(15)
-
-	fmt.Printf()
-	fmt.Println(a1, a2)
+	firstName, lastName := getInit("Pares iqbal")
+	fmt.Println(firstName, lastName)
 }
