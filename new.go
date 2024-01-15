c58c1e300489c2bb705f76dcdba191ee91d29ae3
paresiqbal <thecrowdel@gmail.com>
2024-01-15 21:55:12 +0700

    asdasd

--- new.go
+++ new.go
@@ -3,23 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var firstName string = "Pares"
-	var midleName = "Iqbal"
-	var lastName string
+	name := []string{"Peter", "Paul", "Mary"}
 
-	fmt.Println(firstName, midleName, lastName)
-
-	lastName = "Prastowo"
-
-	fmt.Println(firstName, midleName, lastName)
-
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	var scoreOne float32 = 25.90
-
-	fmt.Println(scoreOne)
+	for index, value := range name {
+		fmt.Printf("position at index %v is %v", index, value)
+	}
 }
