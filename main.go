4301ec1653482c119077394e1cc8db5b32cff494
paresiqbal <thecrowdel@gmail.com>
2024-01-17 20:25:21 +0700

    Maps

--- main.go
+++ main.go
@@ -12,4 +12,21 @@ func main() {
 
 	fmt.Println(menu)
 	fmt.Println(menu["soup"])
+
+	for key, value := range menu {
+		fmt.Println(key, value)
+	}
+
+	phoneBook := map[int]string{
+		100: "John",
+		200: "Paul",
+		300: "George",
+		400: "Ringo",
+	}
+
+	fmt.Println(phoneBook)
+	fmt.Println(phoneBook[100])
+
+	phoneBook[200] = "Pares"
+	fmt.Println(phoneBook)
 }
