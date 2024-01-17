10608b5f1dbae3305855580ec46fd59265ffac0d
paresiqbal <thecrowdel@gmail.com>
2024-01-17 22:31:39 +0700

    asdasd

--- bill.go
+++ bill.go
@@ -5,15 +5,15 @@ import "fmt"
 type bill struct {
 	name  string
 	items map[string]float64
-	tips  float64
+	tip   float64
 }
 
 // make bills
 func newBill(name string) bill {
 	invoice := bill{
 		name:  name,
-		items: map[string]float64{"item1": 100.2, "item2": 200.7},
-		tips:  0,
+		items: map[string]float64{},
+		tip:   0,
 	}
 
 	return invoice
@@ -30,8 +30,21 @@ func (invoice bill) formatBill() string {
 		total += value
 	}
 
+	// add tip
+	fs += fmt.Sprintf("%v Rp...%0.2f", "tip:", invoice.tip)
+
 	// total bill
-	fs += fmt.Sprintf("%v Rp...%0.2f", "total:", total)
+	fs += fmt.Sprintf("%v Rp...%0.2f", "total:", total+invoice.tip)
 
 	return fs
+}
+
+// update tip
+func (invoice *bill) updateTip(tip float64) {
+	invoice.tip = tip
+}
+
+// add item to bill
+func (invoice *bill) addItem(name string, price float64) {
+	invoice.items[name] = price
 }
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ func main() {
 	myBill := newBill("Pares invoice:")
 
 	myBill.formatBill()
+	myBill.addItem("Coffee", 10000)
+	myBill.updateTip(10)
 
 	fmt.Println(myBill.formatBill())
 }
