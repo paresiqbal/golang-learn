069722695de414477309c1205378c8b93a69e4a3
paresiqbal <thecrowdel@gmail.com>
2024-01-17 22:58:34 +0700

    Update

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import "fmt"
+func createBill() bill {
+
+}
 
 func main() {
-	myBill := newBill("Pares invoice:")
-
-	myBill.formatBill()
-	myBill.addItem("Coffee", 10000)
-	myBill.updateTip(10)
-
-	fmt.Println(myBill.formatBill())
+	myBill := createBill()
 }
